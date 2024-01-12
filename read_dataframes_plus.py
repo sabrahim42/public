75e2e9ed87204fbdc472e19302b126b542637b27
Sofia Abrahim <27420137759@buenosaires.gob.ar>
2024-01-12 14:15:37 -0300

    Agregue funciones get_dataframe_file_paths para obtener todas las direcciones de archivos de una carpeta en base y get_dataframes para obtenerun diccionario de dataframes a partir de una lista de direcciones de archivos. De esta forma la funcion get_dataframes_from_folder sigue funcionando igual pero quedan abiertas más opciones para leer dataframes en conjunto. También cree funciones que permiten extraer los parametros de csvs para que la funcion read_file sea más flexible.

--- read_dataframes_plus.py
+++ read_dataframes_plus.py
@@ -1,11 +1,27 @@
 import pandas as pd
 import geopandas as gpd  
 import os
+import pathlib
+import csv
+import chardet
+
+def get_csv_encoding(file_path):
+    with open(file_path, 'rb') as f:
+        result = chardet.detect(f.read())
+    return result['encoding']
+
+def get_csv_info(file_path): 
+    encoding = get_csv_encoding(file_path) 
+    with open(file_path, 'r', encoding = encoding) as f:
+        dialect = csv.Sniffer().sniff(f.read())
+    return {'delimiter': dialect.delimiter, 'quotechar': dialect.quotechar, 'encoding': encoding}  
 
 def read_file(file_path,csv_parameters = {}):
    _, ext = os.path.splitext(file_path)
 
-   if ext == '.csv':
+   if ext == '.csv':       
+       if csv_parameters == {}:
+           csv_parameters = get_csv_info(file_path)
        df = pd.read_csv(file_path,**csv_parameters)
    elif ext == '.xlsx':
        df = pd.read_excel(file_path)
@@ -14,11 +30,20 @@ def read_file(file_path,csv_parameters = {}):
 
    return df if 'df' in locals() else gdf
 
-def get_dataframes_from_folder(folder_path, ignore_extension = (), csv_parameters = {}):
-    folder_dfs = {}
-    for file in os.listdir(folder_path):
-        if os.path.splitext(file)[1] in ignore_extension:
-            continue
-        data = read_file(os.path.join(folder_path,file),csv_parameters=csv_parameters)             
-        folder_dfs[file] = data
+def get_dataframe_file_paths(folder_path,ignore_extension = ()):
+    dir = pathlib.Path(folder_path)
+    file_paths = [f for f in dir.iterdir() if f.suffix not in ignore_extension]
+    return file_paths
+
+def get_dataframes(file_paths: list, csv_parameters = {}):
+    dataframes = {}
+    for file_path in file_paths:       
+        data = read_file(file_path,csv_parameters=csv_parameters) 
+        file_name = os.path.basename(file_path)            
+        dataframes[file_name] = data
+    return dataframes
+
+def get_dataframes_from_folder(folder_path,ignore_extension = (),csv_parameters = {}):
+    file_paths = get_dataframe_file_paths(folder_path,ignore_extension = ignore_extension)
+    folder_dfs = get_dataframes(file_paths, csv_parameters = csv_parameters)
     return folder_dfs
